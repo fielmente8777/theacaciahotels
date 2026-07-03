6eea565277dc64bde7a050411a3ddb04c9340732
Personal <sk7627577@gmail.com>
2026-07-03 14:43:40 +0530

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,12 +82,37 @@ export default function RootLayout({
 
             gtag('config', 'G-1W5EZ5KY14');`}
         </Script>
+
+        {/* <!-- Google Tag Manager --> */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WSDTN8VH');`,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
       </head>
 
       <body
         className={` ${jost.variable} h-full antialiased`}
         suppressHydrationWarning={true}
       >
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WSDTN8VH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <WebProvider>
           {children}
           <Whatsapp whatsAppNumber={contact.phone[0]} />
